47df8a357af20704f6cf54c8b91015dad061df32
Saravanan Vijayappan <sarav@retailcloud.com>
2026-02-24 15:40:41 -0800

    test message

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+       *** This is a test ***
       </p>
     </>
   )
